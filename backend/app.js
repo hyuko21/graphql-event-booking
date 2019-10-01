de301af3d66bf6053070ad52750730b9e03b83cb
victor <victorwdr@gmail.com>
2019-10-01 15:36:36 -0300

    implemented types of event and eventInput - video #4

--- backend/app.js
+++ backend/app.js
@@ -5,16 +5,33 @@ const { buildSchema } = require('graphql');
 
 const app = express();
 
+const events = [];
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use('/graphql', graphqlHttp({
   schema: buildSchema(`
+    type Event {
+      _id: ID!
+      title: String!
+      description: String!
+      price: Float!
+      date: String!
+    }
+
+    input EventInput {
+      title: String!
+      description: String!
+      price: Float!
+      date: String!
+    }
+
     type RootQuery {
-      events: [String!]!
+      events: [Event!]!
     }
 
     type RootMutation {
-      createEvent(name: String): String
+      createEvent(eventInput: EventInput): Event
     }
 
     schema {
@@ -24,11 +41,20 @@ app.use('/graphql', graphqlHttp({
   `),
   rootValue: {
     events: () => {
-      return ['Romantic Cooking', 'Sailing', 'All-Night Coding'];
+      return events;
     },
-    createEvent: args => {
-      const eventName = args.name;
-      return eventName;
+    createEvent: ({ eventInput }) => {
+      const event = {
+        _id: Math.random().toString(),
+        title: eventInput.title,
+        description: eventInput.description,
+        price: +eventInput.price,
+        date: eventInputd.date
+      };
+
+      events.push(event);
+
+      return event
     }
   },
   graphiql: true
