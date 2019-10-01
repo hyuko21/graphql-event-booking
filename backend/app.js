1def57249607fd341f887254daf6d6b2b04a7db3
victor <victorwdr@gmail.com>
2019-10-01 15:05:14 -0300

    implemented basic application of graphql concepts - video #3

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,37 @@
 const express = require('express');
+const graphqlHttp = require('express-graphql');
+
+const { buildSchema } = require('graphql');
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use('/graphql', graphqlHttp({
+  schema: buildSchema(`
+    type RootQuery {
+      events: [String!]!
+    }
 
-app.get('/', (req, res, next) => {
-  res.send('Hello World!');
-});
+    type RootMutation {
+      createEvent(name: String): String
+    }
+
+    schema {
+      query: RootQuery
+      mutation: RootMutation
+    }
+  `),
+  rootValue: {
+    events: () => {
+      return ['Romantic Cooking', 'Sailing', 'All-Night Coding'];
+    },
+    createEvent: args => {
+      const eventName = args.name;
+      return eventName;
+    }
+  },
+  graphiql: true
+}));
 
 app.listen(3000);
