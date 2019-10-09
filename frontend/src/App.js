aaf7f25d42847c46f0945bf0a0eb1f5b279f9c87
victor <victorwdr@gmail.com>
2019-10-09 17:08:07 -0300

    add navbar - video #13

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,28 @@
-import React from 'react';
-import {
-  BrowserRouter,
-  Route,
-  Redirect,
-  Switch
-} from 'react-router-dom';
+import React from 'react'
+import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom'
 
-import AuthPage from './pages/Auth';
-import EventsPage from './pages/Events';
-import BookingsPage from './pages/Bookings';
+import AuthPage from './pages/Auth'
+import EventsPage from './pages/Events'
+import BookingsPage from './pages/Bookings'
+import Navigation from './components/Navigation'
 
-import './App.css';
+import './App.css'
 
 function App() {
   return (
     <BrowserRouter>
-      <Switch>
-        <Redirect from="/" to="/auth" exact />
-        <Route path="/auth" component={AuthPage} />
-        <Route path="/events" component={EventsPage} />
-        <Route path="/bookings" component={BookingsPage} />
-        <Redirect from="*" to="/" exact />
-      </Switch>
+      <Navigation />
+      <main className='main-content'>
+        <Switch>
+          <Redirect from='/' to='/auth' exact />
+          <Route path='/auth' component={AuthPage} />
+          <Route path='/events' component={EventsPage} />
+          <Route path='/bookings' component={BookingsPage} />
+          <Redirect from='*' to='/' exact />
+        </Switch>
+      </main>
     </BrowserRouter>
-  );
+  )
 }
 
-export default App;
+export default App
