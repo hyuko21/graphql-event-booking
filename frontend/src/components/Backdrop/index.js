d94e97b00932bb8bdb140214a5406222954d038c
victor <victorwdr@gmail.com>
2019-10-14 17:49:19 -0300

    create modal component to create events

---
+++ frontend/src/components/Backdrop/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+import './styles.css'
+
+const Backdrop = props => <div className='backdrop'></div>
+
+export default Backdrop
--- frontend/src/pages/Events/index.js
+++ frontend/src/pages/Events/index.js
@@ -1,7 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+import Modal from '../../components/Modal'
+import Backdrop from '../../components/Backdrop'
+
+import './styles.css'
 
 function EventsPage() {
-  return <h1>The Events Page</h1>
+  const [isCreatingEvent, setIsCreatingEvent] = useState(false)
+
+  const startCreatingEvent = () => {
+    setIsCreatingEvent(true)
+  }
+
+  const modalConfirmHandler = () => {
+    setIsCreatingEvent(false)
+  }
+
+  const modalCancelHandler = () => {
+    setIsCreatingEvent(false)
+  }
+
+  return (
+    <>
+      {isCreatingEvent && (
+        <>
+          <Backdrop />
+          <Modal title='Add Event' onCancel={modalCancelHandler} onConfirm={modalConfirmHandler}>
+            <p>Modal Content</p>
+          </Modal>
+        </>
+      )}
+      <div className='events-control'>
+        <p>Share your own Events!</p>
+        <button className='btn' onClick={startCreatingEvent}>
+          Create Event
+        </button>
+      </div>
+    </>
+  )
 }
 
 export default EventsPage
